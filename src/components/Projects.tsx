88c3eb6d9d0703912c32714ef53568532beb8487
satyamregmi6 <satyamregmi6@gmail.com>
2025-06-13 04:48:11 +0000

    Uploaded projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,6 +8,24 @@ import { FiExternalLink, FiGithub } from 'react-icons/fi';
 const projectsData = [
   {
     id: 1,
+    title: 'LLAMA Web Builder',
+    description: 'Build Websites from Images and Voice command with AI',
+    image: 'https://satym.me/image.png',
+    tags: ['Web', 'AI', 'Next.js', 'LLAMA'],
+    demoLink: 'https://llama.satym.me/',
+    codeLink: 'https://github.com/regmisatyam/llama-web-agent',
+  },
+  {
+    id: 2,
+    title: 'GAgent',
+    description: 'Smart Personal Assistant for Google Services and Daily Tasks.',
+    image: 'https://www.teletechnepal.com/favicon.ico',
+    tags: ['AI', 'Web', 'Next.js', 'Node.js', 'Firebase', 'GAuth'],
+    demoLink: 'https://ai.satym.me/',
+    codeLink: 'https://github.com/regmisatyam/llm-agent',
+  },
+  {
+    id: 3,
     title: 'Teletech Nepal',
     description: 'A full-stack blogging platform with auth, user and admin dashboard.',
     image: 'https://www.teletechnepal.com/favicon.ico',
@@ -16,7 +34,7 @@ const projectsData = [
     codeLink: '#',
   },
   {
-    id: 2,
+    id: 4,
     title: 'Guide of Visa',
     description: 'Website that guides through visa process for different countries. Tracks Application and AI powered visa Interview. ',
     image: 'https://guideofvisa.com/favicons/favicon.ico',
@@ -25,7 +43,7 @@ const projectsData = [
     codeLink: '#',
   },
   {
-    id: 3,
+    id: 5,
     title: 'All Nepali News',
     description: 'All Nepali News. Short Fast Authentic news application.',
     image: 'https://play-lh.googleusercontent.com/sK0n5FqGV4Ilc6UEV4T7LdHU6A2Td2uvJJOU1W739S5xcw61t9rdalgbRot4k8VwmPU=w480-h960-rw',
@@ -34,7 +52,7 @@ const projectsData = [
     codeLink: '#',
   },
   {
-    id: 4,
+    id: 6,
     title: 'Fav Icon From Url',
     description: 'Extract website favicons from url and provides favicon link in json format',
     image: '',
@@ -43,7 +61,7 @@ const projectsData = [
     codeLink: 'https://github.com/regmisatyam/FavIconFromUrl',
   },
   {
-    id: 5,
+    id: 7,
     title: 'SBP',
     description: 'SBP is a Content Management System build on pure php with MySQL db. It is fast and occupies low storage. Can run on any apache or nginx server.',
     image: '',
@@ -52,7 +70,7 @@ const projectsData = [
     codeLink: 'https://github.com/regmisatyam/sbp',
   },
   {
-    id: 6,
+    id: 8,
     title: 'Text to Speech',
     description: 'Converts any language text to Speech. Accessible through huggingface studio and API',
     image: '',
